2c86828140009572e80d3153d816434c6daba902
Nash Aguayo <nash.aguayo@gmail.com>
2023-08-10 20:24:57 -0300

    FEAT: Configured aliases

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/pokedex/' : '/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  },
+  transpileDependencies: true,
+});
