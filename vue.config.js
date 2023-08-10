a7b19e9a574fc4ecfddaeafa484070a0d2c7842c
nashaguayo <77163978+nashaguayo@users.noreply.github.com>
2023-08-10 20:39:13 -0300

    [SW-4] Configure tests (#5)
    * FEAT: Installed vue router
    
    * FEAT: Configured jest
    
    * CHORE: Changelog

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,5 @@ const path = require('path');
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/pokedex/' : '/',
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
-    },
-  },
   transpileDependencies: true,
 });
