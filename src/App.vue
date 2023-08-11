38b196af5103797ac4ae7e12b00b1a1fc5cf26dc
Nash Aguayo <nash.aguayo@gmail.com>
2023-08-10 22:27:31 -0300

    FEAT: First fonts, logo and background

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
   <div id="app">
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </nav>
+    </nav> -->
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@font-face {
+  font-family: 'Roboto Condensed';
+  src: local('Roboto Condensed'),
+    url(@/assets/fonts/roboto-condensed.ttf) format('truetype');
+}
+
+@font-face {
+  font-family: 'Nanum';
+  src: local('Nanum'), url(@/assets/fonts/nanum-myeongjo.ttf) format('truetype');
+}
+
+@font-face {
+  font-family: 'Zilla Slab';
+  src: local('Zilla Slab'),
+    url(@/assets/fonts/zilla-slab.ttf) format('truetype');
+}
+
 #app {
+}
+
+body {
+  margin: 0;
+}
+
+h1 {
+  margin: 0 0 1rem;
+  color: white;
+  font-family: 'Zilla Slab';
+}
+
+h2 {
+  margin: 0 0 1rem;
+  color: white;
+  font-family: 'Nanum';
+}
+
+span,
+p {
+  color: white;
+  font-family: 'Roboto Condensed';
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home-view">
-    <h1>This is the Home page</h1>
+    <img src="@/assets/images/ui/logo.svg" />
+    <h1>SkyWatcher</h1>
+    <h2>The universe at the tip of your fingers</h2>
+    <span
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum?
+      Rerum voluptatum doloremque ex! Sunt dolorum sapiente dicta. Possimus ut
+      deleniti voluptas ad quisquam odit quas natus minima doloribus
+      ipsam?</span
+    >
   </div>
 </template>
 
@@ -9,3 +17,11 @@ export default {
   name: 'HomeView',
 };
 </script>
+
+<style lang="scss" scoped>
+.home-view {
+  background-image: url('@/assets/images/landing/hero.jpg');
+  background-size: cover;
+  background-position: center;
+}
+</style>
