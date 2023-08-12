d0ea448363e1aedd26d3227abb63da8718dc56af
Nash Aguayo <nash.aguayo@gmail.com>
2023-08-11 21:11:37 -0300

    FIX: Project config in github pages

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/pokedex/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/skywatcher/' : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
