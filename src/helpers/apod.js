fb3b4ed75daf4ee0bcfaff632bcda6fe53dc5d51
Nash Aguayo <nash.aguayo@gmail.com>
2023-08-11 22:32:42 -0300

    FIX: Wrongly called function

--- src/helpers/apod.js
+++ src/helpers/apod.js
@@ -1,5 +1,5 @@
 import { getAstronomyPictureOfTheDay as getAstronomyPictureOfTheDayApi } from '@/api/nasa/planetary';
 
 export async function getAstronomyPictureOfTheDay() {
-  return await getAstronomyPictureOfTheDay();
+  return await getAstronomyPictureOfTheDayApi();
 }
