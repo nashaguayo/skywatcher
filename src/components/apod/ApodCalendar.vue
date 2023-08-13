e0bd45ab960d8f8c7a51028515b8fe7163ecfeb0
Nash Aguayo <nash.aguayo@gmail.com>
2023-08-13 19:07:18 -0300

    FEAT: First styles

--- src/components/apod/ApodCalendar.vue
+++ src/components/apod/ApodCalendar.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="apod-calendar">
-    <div class="apod-calendar-container"></div>
+    <div class="apod-calendar-container">
+      <h1 class="title">Past APODs</h1>
+      <div class="calendar"></div>
+    </div>
   </div>
 </template>
 
@@ -28,6 +31,29 @@ export default {
 <style lang="scss" scoped>
 .apod-calendar {
   .apod-calendar-container {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: linear-gradient(
+      100deg,
+      var(--secondary-background-color),
+      var(--secondary-gradient-background-color)
+    );
+
+    .title {
+      color: var(--secondary-text-color);
+      margin-bottom: 1rem;
+    }
+
+    .calendar {
+      background-color: var(--variant-background-color);
+      display: grid;
+      grid-template-columns: repeat(7, 1fr);
+      width: calc(100% - 4rem);
+      height: 10rem;
+    }
   }
 }
 </style>
