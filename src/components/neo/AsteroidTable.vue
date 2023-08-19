701f11538ccd628bf25abb39aa695a0caea1238d
nashaguayo <77163978+nashaguayo@users.noreply.github.com>
2023-08-19 13:30:13 -0300

    [SW-37] Drop shadow effect in NEO page list (#35)
    * FEAT: Created bottom shadow for input
    
    * FEAT: Showing shadow only when user has scrolled
    
    * FEAT: Throttling function

--- src/components/neo/AsteroidTable.vue
+++ src/components/neo/AsteroidTable.vue
@@ -5,9 +5,11 @@
       type="date"
       name="date"
       @inputValueChanged="newDate"
+      class="input"
+      :class="{ 'drop-shadow': hasScrolled }"
     />
     <AsteroidTableSkeleton v-if="!loaded" />
-    <div v-else class="asteroid-table-container">
+    <div v-else class="asteroid-table-container" ref="table">
       <AsteroidTableItem
         v-for="neo in neos"
         :key="neo.id"
@@ -23,6 +25,7 @@
 import BaseInput from '@/components/ui/BaseInput.vue';
 import AsteroidTableSkeleton from '@/skeleton/neo/AsteroidTableSkeleton.vue';
 import AsteroidTableItem from '@/components/neo/AsteroidTableItem.vue';
+import throttle from 'lodash/throttle';
 
 export default {
   name: 'AsteroidTable',
@@ -53,9 +56,32 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      hasScrolled: false,
+    };
+  },
+  created() {
+    this.throttledHandleScroll = throttle(this.handleScroll, 500);
+  },
+  watch: {
+    async loaded(loaded) {
+      await this.$nextTick();
+      if (loaded) {
+        this.$refs.table.addEventListener('scroll', this.throttledHandleScroll);
+      }
+    },
+  },
   methods: {
     newDate(date) {
+      this.$refs.table.removeEventListener(
+        'scroll',
+        this.throttledHandleScroll
+      );
       this.$emit('newDate', date);
+    },
+    handleScroll() {
+      this.hasScrolled = this.$refs.table.scrollTop !== 0;
     },
   },
 };
@@ -70,5 +96,18 @@ export default {
   );
   width: 100%;
   margin-top: 1rem;
+
+  .drop-shadow {
+    box-shadow: var(--bottom-box-shadow);
+  }
+
+  .input {
+    transition: box-shadow 0.3s;
+  }
+
+  .asteroid-table-container {
+    overflow-y: scroll;
+    max-height: calc(100vh - 10rem);
+  }
 }
 </style>
