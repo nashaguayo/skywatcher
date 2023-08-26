330a9aca99eb48aee9fa6fad6fdb823250572b66
Nash Aguayo <nash.aguayo@gmail.com>
2023-08-26 15:12:32 -0300

    FEAT: Created installing page

---
+++ tests/unit/views/InstallingView.spec.js
@@ -0,0 +1,38 @@
+import { shallowMount } from '@vue/test-utils';
+import InstallingView from '@/views/InstallingView.vue';
+
+jest.mock('@/components/ui/BaseSpinner.vue', () => ({
+  name: 'BaseSpinner',
+  template: '<div class="mock-base-spinner"></div>',
+}));
+
+describe('InstallingView', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(InstallingView, { stubs: ['BaseSpinner'] });
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it('renders the component', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('contains the "installing" class', () => {
+    expect(wrapper.classes()).toContain('installing-view');
+  });
+
+  it('renders everything correctly', () => {
+    const title = wrapper.find('h1');
+    expect(title.exists()).toBeTruthy();
+    expect(title.text()).toBe('Installing...');
+
+    expect(wrapper.find('basespinner-stub').exists()).toBeTruthy();
+  });
+});
